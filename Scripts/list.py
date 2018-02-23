bac282b7bb7594663e81145e75d9b1c9baf21d66
hkavana <36750484+hkavana@users.noreply.github.com>
2018-02-23 12:47:01 -0500

    List File created

---
+++ Scripts/list.py
@@ -0,0 +1,12 @@
+lis = ["Kavana", 123, "Sanjana",456,"Spoorthi",789]
+num_lis = []
+str_lis = []
+for i in lis:
+    if isinstance(i, int) or isinstance(i, float):
+        num_lis.append(i)
+    elif isinstance(i, string):
+        str_lis.append(i)
+    else:
+        pass
+print(num_lis,str_lis)
+
