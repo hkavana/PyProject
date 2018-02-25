36ba3c3c349207c7dadba59f8a07236ee5d7cd29
hkavana <36750484+hkavana@users.noreply.github.com>
2018-02-25 15:09:24 -0500

    Inheritence program modidied - check @property

--- Scripts/Inheritence.py
+++ Scripts/Inheritence.py
@@ -2,12 +2,15 @@ class Animals():
     color = "Black"
     size = "Large"
     noise = "Grunts"
+    def get_color(self,color):
+        return self.color + " " + color
     def makes_noise(self):
         return self.noise
 class Dog(Animals):
     name = "Champ"
     color = "Golden"
     noise = "Barks"
+    @property
     def makes_noise(self):
         return self.noise
 
@@ -15,11 +18,13 @@ an = Animals()
 print(an.color)
 print(an.size)
 print(an.noise)
+print(an.get_color("White"))
 print(an.makes_noise())
 
 dog = Dog()
 print(dog.name)
 print(dog.color)
 print(dog.noise)
-print(dog.makes_noise())
+print(dog.get_color("Brown"))
+print(dog.makes_noise))
 
